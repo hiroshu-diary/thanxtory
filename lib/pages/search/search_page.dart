ace731d229d3264a4c91485fd8a62b4a75ebdbe0
“hiroshu-diary” <hiroshu.diary@gmail.com>
2022-03-11 10:40:22 +0900

    Future<String>で受け取った値を検索値が含んでいる時にヒットする検索機能

--- lib/pages/search/search_page.dart
+++ lib/pages/search/search_page.dart
@@ -1,6 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
+import '../../model/constant.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({
@@ -13,7 +18,15 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final _controller = FloatingSearchBarController();
+  final _userProfiles = FirebaseFirestore.instance.collection('userProfiles');
   late TextEditingController _searchController;
+  final _storage = FirebaseStorage.instance;
+  Future<String> getURL(String id) async {
+    var ref = _storage.ref('$id/default_image.jpeg');
+    String imageUrl = await ref.getDownloadURL();
+    return imageUrl;
+  }
 
   @override
   void initState() {
@@ -30,7 +43,6 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // This is handled by the search bar itself.
       resizeToAvoidBottomInset: false,
       body: Stack(
         fit: StackFit.expand,
@@ -46,6 +58,7 @@ class _SearchPageState extends State<SearchPage> {
         MediaQuery.of(context).orientation == Orientation.portrait;
 
     return FloatingSearchBar(
+      controller: _controller,
       hint: '誰をさがす？',
       scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
       transitionDuration: const Duration(milliseconds: 800),
@@ -55,11 +68,8 @@ class _SearchPageState extends State<SearchPage> {
       openAxisAlignment: 0.0,
       width: isPortrait ? 600 : 500,
       debounceDelay: const Duration(milliseconds: 500),
-      onQueryChanged: (query) {
-        // Call your model, bloc, controller here.
-      },
-      // Specify a custom transition to be used for
-      // animating between opened and closed stated.
+      onQueryChanged: (String text) {},
+      onSubmitted: (String text) {},
       transition: CircularFloatingSearchBarTransition(),
       actions: [
         FloatingSearchBarAction(
@@ -74,19 +84,112 @@ class _SearchPageState extends State<SearchPage> {
         ),
       ],
       builder: (context, transition) {
-        //FutureBuilder
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Material(
-            color: Colors.white,
-            elevation: 4.0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: Colors.accents.map((color) {
-                return Container(height: 112, color: color);
-              }).toList(),
-            ),
-          ),
+        return FutureBuilder<QuerySnapshot>(
+          future: _userProfiles.get(),
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<QuerySnapshot> snapshot,
+          ) {
+            List<String> returnList = [];
+            _userProfiles.get().then((QuerySnapshot snapshot) {
+              snapshot.docs.forEach((doc) async {
+                String _name = await doc.get('name') as String;
+                if (_controller.query.contains(_name)) {
+                  returnList.add(doc.id);
+                }
+              });
+            });
+            if (snapshot.hasError) {
+              return const Text('ヒット件数が０件でした。');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  child: SizedBox(
+                    width: 240,
+                    height: 240,
+                    child: CircularProgressIndicator(
+                      color: C.subColor,
+                    ),
+                  ),
+                ),
+              );
+            }
+
+            return ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: returnList.length,
+              itemBuilder: (context, int index) {
+                return ConstrainedBox(
+                  constraints: const BoxConstraints(minWidth: double.maxFinite),
+                  child: Card(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.transparent,
+                            radius: 33,
+                            child: ClipOval(
+                              child: FutureBuilder(
+                                future: getURL(returnList[index]),
+                                builder: (BuildContext context,
+                                    AsyncSnapshot<String> snapshot) {
+                                  if (snapshot.connectionState ==
+                                          ConnectionState.waiting ||
+                                      !snapshot.hasData) {
+                                    return CachedNetworkImage(
+                                      fit: BoxFit.cover,
+                                      imageUrl: snapshot.data!,
+                                    );
+                                  }
+                                  return Container();
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        FutureBuilder(
+                          future: _userProfiles.doc(returnList[index]).get(),
+                          builder: (
+                            BuildContext context,
+                            AsyncSnapshot<DocumentSnapshot> snapshot,
+                          ) {
+                            if (snapshot.hasData) {
+                              Map<String, dynamic> data =
+                                  snapshot.data!.data() as Map<String, dynamic>;
+
+                              return Text(
+                                data['name'].toString(),
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: 'NotoSansJP',
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              );
+                            }
+                            return const Text('　　');
+                          },
+                        ),
+                        const Spacer(),
+                        GestureDetector(
+                          child: const Text('この人に贈る'),
+                          onTap: () {},
+                        ),
+                        GestureDetector(
+                          child: const Text('詳しく見る'),
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
         );
       },
     );
