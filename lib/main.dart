88ab2b8c9d2f2b5f3d29670bc8bb4208bec84d2c
“hiroshu-diary” <hiroshu.diary@gmail.com>
2022-03-21 16:11:46 +0900

    アプリ削除時、自動サインアウト機能

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:thanxtory/pages/home/home_page.dart';
 import 'package:thanxtory/pages/login/login_page.dart';
 
@@ -10,11 +11,24 @@ import 'model/constant.dart';
 import 'model/scaffold_messenger_controller.dart';
 import 'routes/routes.dart';
 
+var isFirstLaunch = true;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  var _prefs = await SharedPreferences.getInstance();
+  _prefs.setBool('isFirstLaunch', true);
+  var isFirstLaunch = _prefs.getBool('isFirstLaunch') ?? true;
+  if (isFirstLaunch) {
+    try {
+      FirebaseAuth.instance.signOut();
+      _prefs.setBool('isFirstLaunch', false);
+    } on Exception {
+      print('ERROR');
+    }
+  }
 
   runApp(
     MultiProvider(
